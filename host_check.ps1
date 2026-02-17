52a68bfe46111841968aad451bc4591db703cd33
Mike Love <94137977+mlove1223@users.noreply.github.com>
2026-02-17 11:53:04 -0600

    Update sites list in host_check.ps1
    Removed multiple site URLs and added 'site.site.com' three times.

--- host_check.ps1
+++ host_check.ps1
@@ -1,18 +1,8 @@
 $sites = @(
-  "atlas.sram.com",
-  "https://pdm.sram.com/Windchill/app/",
-  "jupyter.sram.com",
-  "gttdbs.sram.com",
-  "https://pdm-qa.sram.com/Windchill/app/",
-  "https://pdm-dv.sram.com/Windchill/app/",
-  "https://kbase.sram.com/",
-  "swiki.sram.com",
-  "psp.sram.com",
-  "serilook.sram.com",
-  "cqf.sram.com/cqfs.php",
-  "https://sram-prod.mdm.stibosystems.com/#",
-  "https://sram-preprod.mdm.stibosystems.com/#",
-  "https://sram-dev.mdm.stibosystems.com/#"
+  "site.site.com",
+  "site.site.com",
+  "site.site.com"
+"
 )
  
 Write-Host "==== HTTPS Check ===="
